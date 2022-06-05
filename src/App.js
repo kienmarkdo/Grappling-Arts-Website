d0eca51e115f592319a80dc1755a6c9a060a9803
Kien Do <kienmarkdo@gmail.com>
2022-06-05 17:01:20 -0400

    Added browser router package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 //* import packages and dependencies
 import "bootstrap/dist/css/bootstrap.min.css"; // react bootstrap library
 
+//* import components
+import { Navbar, Container, Nav } from "react-bootstrap"; // react bootstrap library components
+
 //* Import main pages (sections of website)
 import About from "./pages/About";
 import Instructors from "./pages/Instructors";
@@ -12,7 +15,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <About></About>
+      <Navbar className="color-navbar" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">uOttawa Grappling Arts</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
