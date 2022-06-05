3358b77b5dcfe466f3d61feadb90bca8e86d8ae3
Kien Do <kienmarkdo@gmail.com>
2022-06-05 16:43:16 -0400

    Reorganized directory. Added React Boostrap library with npm install react-bootstrap bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import {
-  About,
-  Programs,
-  LocationsSchedule,
-  Instructors,
-  Membership,
-} from "./pages";
+//* import packages and dependencies
+import "bootstrap/dist/css/bootstrap.min.css"; // react bootstrap library
+
+//* Import main pages (sections of website)
+import About from "./pages/About";
+import Instructors from "./pages/Instructors";
+import LocationsSchedule from "./pages/LocationsSchedule";
+import Membership from "./pages/Membership";
+import Programs from "./pages/Programs";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <About></About>
     </div>
   );
 }
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+export default function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <Button>Press</Button>
+    </>
+  );
+}
